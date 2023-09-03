6e2e679f98206db28b6568d53825c5e3ebf6450f
Iwan Firmawan <firmawaneiwan@gmail.com>
2023-09-03 22:18:12 +0700

    [#1] Update metadata with kakadigi profile

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Karya Kami digital",
+	url: "https://kakadigi.github.io/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Karya Kami Digital",
+		email: "hi@kakadigi.com",
+		url: "https://kakadigi.github.io/blog/"
 	}
 }
